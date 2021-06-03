2f142e7d9b59d99950ef8474e49ba53c16c0c578
Alexandru Codreanu <alexandru.codreanu.04@gmail.com>
2021-06-03 12:58:38 +0300

    Second commit

--- config/database.js
+++ config/database.js
@@ -1,31 +1,30 @@
-const parse = require('pg-connection-string').parse
+const parse = require('pg-connection-string').parse;
 
 module.exports = ({ env }) => {
 
   if(env('NODE_ENV') === 'production'){
-    const config = parse(process.env.DATABASE_URL)
-
+    const config = parse(process.env.DATABASE_URL);
     return {
-      defaultConnection : 'default',
-      connections : {
-        default : {
-          connector : 'bookshelf',
-          setting : {
-            client : 'postgres',
-            host : config.host,
-            port : config.port,
-            database : config.database,
-            username : config.user,
-            password : config.password
+      defaultConnection: 'default',
+      connections: {
+        default: {
+          connector: 'bookshelf',
+          settings: {
+            client: 'postgres',
+            host: config.host,
+            port: config.port,
+            database: config.database,
+            username: config.user,
+            password: config.password,
           },
-          options : {
-            ssl: false
-          }
-        }
-      }
+          options: {
+            ssl: false,
+          },
+        },
+      },
     }
   }
-
+  
   return {
     defaultConnection: 'default',
     connections: {
