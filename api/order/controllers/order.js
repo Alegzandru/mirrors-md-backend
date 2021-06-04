58d136f91dc7438ac2b02048f6c63ab9e2c65ab4
Alexandru Codreanu <alexandru.codreanu.04@gmail.com>
2021-06-04 15:05:40 +0300

    Working on post/update

--- api/order/controllers/order.js
+++ api/order/controllers/order.js
@@ -1,8 +1,38 @@
-'use strict';
+'use strict'
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
+  /**
+   * Create a record.
+   *
+   * @return {Object}
+   */
 
-module.exports = {};
+  async create(ctx) {
+    let entity;
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.order.create(data, { files });
+    } else {
+      entity = await strapi.services.order.create(ctx.request.body);
+    }
+    return sanitizeEntity(entity, { model: strapi.models.order });
+  },
+
+  async update(ctx) {
+    const { id } = ctx.params;
+
+    let entity;
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.order.update({ id }, data, {
+        files,
+      });
+    } else {
+      entity = await strapi.services.order.update({ id }, ctx.request.body);
+    }
+
+    return sanitizeEntity(entity, { model: strapi.models.order });
+  },
+};
+
