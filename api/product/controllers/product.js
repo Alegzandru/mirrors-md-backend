88f42ee61edb839667803c218237b473a3f3f999
Alexandru Codreanu <alexandru.codreanu.04@gmail.com>
2021-06-07 13:12:41 +0300

    Changed image file to images

--- api/product/controllers/product.js
+++ api/product/controllers/product.js
@@ -8,21 +8,21 @@
 const { sanitizeEntity } = require('strapi-utils');
 
 module.exports = {
-   /**
+    /**
     * Retrieve records.
     *
     * @return {Array}
     */
- 
-   async find(ctx) {
-     let entities;
-     if (ctx.query._q) {
-       entities = await strapi.services.product.search(ctx.query);
-     } else {
-       entities = await strapi.services.product.find(ctx.query);
-     }
- 
-     return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.product }));
-   },
+
+    async find(ctx) {
+      let entities;
+      if (ctx.query._q) {
+        entities = await strapi.services.product.search(ctx.query);
+      } else {
+        entities = await strapi.services.product.find(ctx.query);
+      }
+
+      return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.product }));
+    },
 };
  
